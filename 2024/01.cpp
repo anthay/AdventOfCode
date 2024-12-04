5ca93baf8a7c3f0fef7a0c80b90a6b226534f026
Anthony Hay <anthony.hay.1@gmail.com>
2024-12-04 11:06:40 +0000

    Update 01.cpp

--- 2024/01.cpp
+++ 2024/01.cpp
@@ -1,3 +1,5 @@
+// https://adventofcode.com/2024/day/1
+
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -7,28 +9,19 @@
 #include <chrono>
 
 
-int main()
+void solve(std::vector<int> left_list, std::vector<int> right_list,
+    int_least64_t & total_distance, int_least64_t & similarity_score)
 {
-    std::vector<int> left_list, right_list;
-    std::ifstream input("input01.txt");
-    if (!input.is_open())
-        return EXIT_FAILURE;
-    for (int left, right; input >> left >> right;) {
-        left_list.push_back(left);
-        right_list.push_back(right);
-    }
-
-    const auto start_time = std::chrono::high_resolution_clock::now();
+    total_distance = similarity_score = 0;
 
     // part 1
+
     std::ranges::sort(left_list);
     std::ranges::sort(right_list);
-    int_least64_t total_distance = 0;
     for (int i = 0; i < left_list.size(); ++i)
         total_distance += std::abs(left_list[i] - right_list[i]);
 
     // part 2
-    int_least64_t similarity_score = 0;
 
 #ifdef USE_MAP
     std::unordered_map<int, int> right_unique_count;
@@ -51,6 +44,27 @@ int main()
         similarity_score += static_cast<int_least64_t>(left) * (iright - iright_start);
     }
 #endif
+}
+
+
+int main()
+{
+    std::vector<int> left_list, right_list;
+    std::ifstream input("input01.txt");
+    if (!input.is_open())
+        return EXIT_FAILURE;
+    for (int left, right; input >> left >> right;) {
+        left_list.push_back(left);
+        right_list.push_back(right);
+    }
+
+    const auto start_time = std::chrono::high_resolution_clock::now();
+
+    int_least64_t total_distance = 0;
+    int_least64_t similarity_score = 0;
+    const int repetitions = 1000;
+    for (int i = 0; i < repetitions; ++i)
+        solve(left_list, right_list, total_distance, similarity_score);
 
     const std::chrono::duration<double, std::milli> ms = std::chrono::high_resolution_clock::now() - start_time;
 
@@ -58,5 +72,5 @@ int main()
     assert(total_distance == 1258579);
     std::cout << similarity_score << '\n';
     assert(similarity_score == 23981443);
-    std::cout << "(" << ms.count() << " ms)\n";
+    std::cout << "(" << ms.count() / repetitions << " ms)\n";
 }
